12a912cfd0b16cb3f25941bcb92c233c389b0a64
Vijet5467 <93580535+Vijet5467@users.noreply.github.com>
2022-07-08 15:44:04 +0530

    document_details

--- phyDoc_Builder/phyDoc_app/admin.py
+++ phyDoc_Builder/phyDoc_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from phyDoc_app.models import Document_templates, Document_details 
 
 # Register your models here.
+admin.site.register(Document_templates)
+admin.site.register(Document_details)
--- phyDoc_Builder/phyDoc_app/serializer.py
+++ phyDoc_Builder/phyDoc_app/serializer.py
@@ -1,11 +1,14 @@
 from rest_framework import  serializers
 from .models import Document_templates, Document_details
+
 class Document_templatesSerializer(serializers.ModelSerializer):
+    # item = serializers.RelatedField(many=True)
     class Meta:
         model = Document_templates
-        fields = '__all__'
+        fields = ('name','id','Document_template_path')
         
 class Document_detailsSerializer(serializers.ModelSerializer):
+    # item = serializers.RelatedField(source='category', read_only=True)
     class Meta:
         model = Document_details
-        fields = ('id', 'field_name', 'field_type','isRequired')
+        fields = ('id','field_name','field_type','isRequired','templateid')
