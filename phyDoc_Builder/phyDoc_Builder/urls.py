2588cd4a26166757b3ab33d95477f9c31e3edb8a
Vijet5467 <93580535+Vijet5467@users.noreply.github.com>
2022-07-11 19:53:15 +0530

    pdf functionality added to get data from document_template ->database

--- phyDoc_Builder/phyDoc_Builder/urls.py
+++ phyDoc_Builder/phyDoc_Builder/urls.py
@@ -20,6 +20,8 @@ from . import views
 urlpatterns = [
     path('', include('phyDoc_app.urls')),
     path('admin/', admin.site.urls),
-    path('apiend',views.insertTemplate)
+    path('apiend',views.insertTemplate),
+    path('venue_pdf',views.venue_pdf,name='venue_pdf'),
+    
 
 ]
--- phyDoc_Builder/phyDoc_Builder/views.py
+++ phyDoc_Builder/phyDoc_Builder/views.py
@@ -9,6 +9,41 @@ from rest_framework.decorators import api_view
 import requests
 from django.shortcuts import render
 
+#generating a pdf file
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+#pdf
+def venue_pdf(request):
+
+    buf = io.BytesIO()
+    c = canvas.Canvas(buf, pagesize=letter,bottomup=0)
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont("Helvetica",14)
+
+    doc =Document_templates.objects.all()
+    lines = []
+
+    for venue in doc:
+        lines.append(str(venue.id))
+        lines.append(venue.name)
+        lines.append(venue.Document_template_path)
+        lines.append("")
+
+    for line in lines:
+        textob.textLine(line)
+    c.drawText(textob)
+    c.showPage()
+    c.save()  
+    buf.seek(0)  
+
+    return FileResponse(buf,as_attachment=True,filename='venue1.pdf')
+
+
 
 def insertTemplate(request):
     if request.method=="POST":
@@ -20,7 +55,10 @@ def insertTemplate(request):
         read= requests.post('http://127.0.0.1:8000/Document_templates/CreateDT',json=data,headers=headers)
         return render(request,'insert.html')
     else:
-        return render(request,'insert.html')      
+        return render(request,'insert.html') 
+
+
+             
 
 
           
