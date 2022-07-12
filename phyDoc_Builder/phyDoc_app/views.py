7ce20a6b6aab220c7f8d3e82620693c59a39d5f6
Anusha <anusgowda050@gmail.com>
2022-07-12 11:41:42 +0530

    Update views.py

--- phyDoc_Builder/phyDoc_app/views.py
+++ phyDoc_Builder/phyDoc_app/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Document_templates
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the phyDoc index.")
-
-def myview(request):
-    #get your list of cities you may also do some filtering 
-    templatesid = [obj.id for obj in Document_templates.objects.all()]
-    return render(request, 'cities_template.html', {'templatesid': templatesid})
